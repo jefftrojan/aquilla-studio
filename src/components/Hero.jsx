bad28a007873f1dc785c25bf018fa905176342e3
jefftrojan <j.dauda@alustudent.com>
2023-09-09 22:28:40 +0200

    Responsiveness fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,12 +13,12 @@ const Hero = () => {
           {/* <div className="absolute p-12 top-60 left-10 transform text-2xl sm:text-2xl md:text-2xl lg:text-2xl xl:text-3xl mt-8 hidden md:block">
             Welcome to
           </div> */}
-          <div className="text-6xl sm:text-7xl md:text-8xl lg:text-9xl xl:text-10xl font-bold font-orbitron">
-            <span className="text-white sm:px-4 py-1 mr-1 z-10 sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl">AQUILA</span>
-            <span className="bg-white text-black rounded-xl px-2 py-1 relative z-10 text-6xl sm:text-7xl md:text-8xl lg:text-9xl xl:text-10xl">STUDIO</span>
+          <div className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl font-bold font-orbitron">
+            <span className="text-white sm:px-4 py-1 mr-1 z-10 sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl">AQUILA</span>
+            <span className="bg-white text-black rounded-xl px-2 py-1 relative z-10 text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl">STUDIO</span>
           </div>
           {/* Hide "Website" on small and medium screens */}
-          <div className="absolute bottom-70 right-40 p-5 transform text-2xl sm:text-2xl md:text-2xl lg:text-2xl xl:text-3xl font-bold font-orbitron hidden md:block">
+          <div className="absolute bottom-70 right-40 p-5 transform text-lg sm:text-lg md:text-lg lg:text-lg xl:text-xl font-bold font-orbitron hidden md:block">
             Website
           </div>
         </div>
