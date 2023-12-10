e52c035485ce24889d3bec118e747028d79e5220
jefftrojan <j.dauda@alustudent.com>
2023-12-10 20:53:51 +0200

    footer layout

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -15,23 +15,23 @@ const Footer = () => {
         <div className="flex-col justify-start items-start gap-[30px] inline-flex">
             <div className="justify-start items-start gap-5 inline-flex">
                 <div className="flex-col justify-start items-start gap-[22px] inline-flex">
-                    <div className="w-[79px] text-zinc-400 text-2xl font-medium font-['Exo']">Name</div>
-                    <div className="w-[285px] h-[50px] relative bg-white" />
+                    <div className="w-[79px] text-zinc-400 text-2xl font-medium font-['Exo']">First Name</div>
+                    <input className="w-[285px] h-[50px] relative bg-white text-black"/>
                 </div>
                 <div className="flex-col justify-start items-start gap-[22px] inline-flex">
-                    <div className="w-[79px] text-zinc-400 text-2xl font-medium font-['Exo']">Name</div>
-                    <div className="w-[285px] h-[50px] relative bg-white" />
+                    <div className="w-[79px] text-zinc-400 text-2xl font-medium font-['Exo']">Last Name</div>
+                    <input className="w-[285px] h-[50px] relative bg-white text-black" />
                 </div>
             </div>
             <div className="flex-col justify-start items-start gap-5 flex">
                 <div className="flex-col justify-start items-start gap-1.5 flex">
                     <div className="w-[105px] text-zinc-400 text-2xl font-medium font-['Exo']">Message</div>
-                    <div className="w-[590px] h-64 relative bg-white" />
+                    <textarea className="w-[590px] h-64 relative bg-white text-black" />
                 </div>
                 <div className="justify-start items-end gap-[18px] inline-flex">
                     <div className="w-[419px] text-zinc-400 text-lg font-medium font-['Exo']">GD4T is a learning program to empower tech talents to learn the principles of game </div>
                     <div className="h-[50px] pl-[53px] pr-[54px] py-[13px] bg-blue-500 rounded-[10px] justify-center items-center flex">
-                        <div className="text-white text-lg font-semibold font-['Exo']">SEND</div>
+                        <button className="text-white text-lg font-semibold font-['Exo']">SEND</button>
                     </div>
                 </div>
             </div>
